ae04feb9d4052abefc099fb1aed8de84b94383af
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-12-17 00:17:38 +0900

    chore: fonts css 파일 위치 변경

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,7 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import "../style/global.css";
-import fonts from "./styles/fonts.css";
+import fonts from "../style/fonts.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
