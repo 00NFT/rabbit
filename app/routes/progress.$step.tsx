69840dbbc27ad08f80e59cad022e4c8240dd0714
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-20 13:33:14 +0900

    feat: 게임 진행 페이지 마크업

---
+++ app/routes/progress.$step.tsx
@@ -0,0 +1,84 @@
+import { css } from "@emotion/react";
+import { useParams } from "@remix-run/react";
+
+// 1단계 임시 데이터
+const dummy = new Array(4).fill(null).map((_, idx) => <div key={idx}>{idx + 1}번째 이미지</div>);
+
+export default function Game() {
+  const { step } = useParams();
+
+  if (!step) return <div>로딩중</div>;
+
+  return (
+    <>
+      <div css={containerCss}>
+        <h1>다르게 생긴 {}를 찾아줘</h1>
+        <div css={progressBarCss(60)}>
+          <div />
+        </div>
+        <div role="button" css={imageGridCss(Number(step) + 1)}>
+          {dummy.map((d) => d)}
+        </div>
+      </div>
+    </>
+  );
+}
+
+const containerCss = css`
+  height: 100%;
+
+  padding: 0 24px;
+
+  background-color: #f0f4fc;
+  text-align: center;
+
+  > h1 {
+    margin-top: 4px;
+
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 34px;
+  }
+`;
+
+const imageGridCss = (gridCnt: number) => css`
+  display: grid;
+  grid-template-columns: repeat(${gridCnt}, 1fr);
+  gap: 12px;
+
+  max-height: 80%;
+  aspect-ratio: 3/4;
+
+  margin: 28px auto;
+
+  cursor: pointer;
+
+  > div {
+    width: 100%;
+    height: 100%;
+
+    background-color: white;
+    border-radius: 12px;
+  }
+`;
+
+const progressBarCss = (progress: number) => css`
+  width: 100%;
+  height: 6px;
+
+  margin-top: 22px;
+
+  background-color: #dde6fa;
+  border-radius: 100px;
+
+  overflow: hidden;
+
+  > div {
+    width: ${progress}%;
+    height: 100%;
+
+    background-color: #151528;
+
+    transition: width 0.1s linear;
+  }
+`;
