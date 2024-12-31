458ed3211c02f088ee635028bc8cf51fc8a60dd4
klmhyeonwoo <gentlemonster77@likelion.org>
2024-12-31 11:09:11 +0900

    feat: 플레이어 이름 데이터 연동 및 기타 QA 반영

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import "../style/fonts.css";
 import "../style/global.css";
@@ -14,12 +14,22 @@ export const links: LinksFunction = () => [
   },
 ];
 
+export const meta: MetaFunction = () => {
+  return [
+    { property: "og:title", content: "새해맞이 달토끼 구출 대작전" },
+    { property: "og:description", content: "틀린 그림찾기로 달토끼를 구해줘!" },
+    { property: "og:image", content: "/images/og/og.png" },
+    { property: "og:image:width", content: "1200" },
+    { property: "og:image:height", content: "630" },
+  ];
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <Meta />
         <Links />
       </head>
